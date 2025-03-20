6d106811a4b38be13287f3666b2fcc16b9d650ea
chromyy33 <103217154+chromyy33@users.noreply.github.com>
2025-03-21 01:08:28 +0530

    Update cors.js

--- api/cors.js
+++ api/cors.js
@@ -1,19 +1,17 @@
 export function corsMiddleware(handler) {
   return async (req, res) => {
     // Set CORS headers
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization'
     );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
+    res.setHeader('Access-Control-Max-Age', '86400'); // 24 hours
 
     // Handle preflight requests
-    if (req.method === "OPTIONS") {
+    if (req.method === 'OPTIONS') {
       res.status(200).end();
       return;
     }
